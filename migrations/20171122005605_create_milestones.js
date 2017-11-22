40a16a269d966ac425dc24d6cd75725ee09df95d
Karanveer Singh <singhkaranveer671@gmail.com>
2017-11-21 17:25:30 -0800

    Add files via upload

---
+++ migrations/20171122005605_create_milestones.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("milestones", (table) => {
+    table.increments();
+    table.string("description");
+    table.date('date');
+  })
+};
+
+exports.down = function(knex, Promise) {
+ }
+
+
+
